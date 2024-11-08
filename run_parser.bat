3f662a028b2ffae7d6931bbad2c7d9d4cd81d05f
sam's_club <113044463+samshensclub@users.noreply.github.com>
2024-11-08 09:54:21 +0800

    Add files via upload

---
+++ run_parser.bat
@@ -0,0 +1,5 @@
+@echo off
+
+C:\Users\s84387544\AppData\Local\Programs\Python\Python313\python.exe C:\Users\s84387544\Desktop\ResumeCLT-main\ResumeCLT.py --source_dir "C:\Users\s84387544\Desktop\ResumeCLT-main\input" --output_dir "C:\Users\s84387544\Desktop\ResumeCLT-main\output" --target_list "C:\Users\s84387544\Desktop\ResumeCLT-main\target_school_list.txt"
+
+pause
